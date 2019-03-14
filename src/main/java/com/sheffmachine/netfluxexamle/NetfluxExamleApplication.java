13c0e0c1ee6a285a401741b34b2623da33c9faae
SHEFFmachine <sheff@sheffmachine.com>
2019-03-13 22:01:02 -0700

    First commit, get a project setup.

---
+++ src/main/java/com/sheffmachine/netfluxexamle/NetfluxExamleApplication.java
@@ -0,0 +1,13 @@
+package com.sheffmachine.netfluxexamle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxExamleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetfluxExamleApplication.class, args);
+    }
+
+}
